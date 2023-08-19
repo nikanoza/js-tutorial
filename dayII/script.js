0ad88087885ef13012dad4b59fe3c6811a46e677
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-19 14:14:55 +0400

    wip: work with day2

---
+++ dayII/script.js
@@ -0,0 +1,39 @@
+// const name = "NIKA";
+
+// const upper = name.toUpperCase();
+// const lower = name.toLowerCase();
+
+// const string = "My name is Nika";
+
+// const sliced = string.slice(3, 7);
+
+// const contain = string.includes("name");
+
+const challenge = "Many Days Of JavaScript";
+
+console.log(challenge);
+console.log(challenge.length);
+
+const upper = challenge.toUpperCase();
+console.log(upper);
+
+const lower = challenge.toLowerCase();
+console.log(lower);
+
+const sliced = challenge.slice(0, 4);
+console.log(sliced);
+
+const slicedPhrase = challenge.slice(5);
+console.log(slicedPhrase);
+
+const contain = challenge.includes("Script");
+console.log(contain);
+
+// Declare a variable named challenge and assign it to an initial value 'Many Days Of JavaScript'.
+// Print the string on the browser console using console.log()
+// Print the length of the string on the browser console using console.log()
+// Change all the string characters to capital letters using toUpperCase() method
+// Change all the string characters to lowercase letters using toLowerCase() method
+// Cut (slice) out the first word of the string using slice() method
+// Slice out the phrase Days Of JavaScript from Many Days Of JavaScript.
+// Check if the string contains a word Script using includes() method
